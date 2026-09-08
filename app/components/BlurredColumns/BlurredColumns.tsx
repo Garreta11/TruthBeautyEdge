5596c7b2664292e0d763996e4f5da38dbf012184
garreta11 <jordigarreta11@gmail.com>
2026-09-08 18:27:43 +0200

    remove layer

--- app/components/BlurredColumns/BlurredColumns.tsx
+++ app/components/BlurredColumns/BlurredColumns.tsx
@@ -1,8 +1,14 @@
+'use client'
+
+import { usePanel } from '@/app/context/PanelContext'
 import styles from "./BlurredColumns.module.scss"
 
 export default function BlurredColumns() {
+  const { openPanel } = usePanel()
+  const isInfoOpen = openPanel === 'info'
+
   return (
-    <div className={styles.columns}>
+    <div className={`${styles.columns} ${isInfoOpen ? styles.behind : ''}`}>
       <div className={styles.columns__left}>
         <div className={`${styles.columns__layer} ${styles.columns__layer1}`} />
         <div className={`${styles.columns__layer} ${styles.columns__layer2}`} />
